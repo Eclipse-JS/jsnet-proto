ec0e90ededdcc6dd64db121e3f801430e1755e05
greysoh <dev@hofers.cloud>
2023-03-13 19:41:48 -0400

    feature(ws-server): Adds basic IP reservation support.
    As well as a client list.

--- ws-server/index.mjs
+++ ws-server/index.mjs
@@ -6,8 +6,43 @@ import { readFile } from "fs/promises";
 
 const config = JSON.parse(await readFile("./config.json"));
 
+const clients = [];
+const reserveIPs = [];
+
 const app = express();
 const server = createServer(app);
+
+app.use(express.json());
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+app.get("/api/v1/getClientList", function(req, res) {
+  res.send({
+    success: true,
+    data: clients // TODO: sanitize(?)
+  })
+});
+
+app.post("/api/v1/reserveClientIP", function(req, res) {
+  const targetPass = getRandomArbitrary(10000000, 99999999);
+  let targetIP = getRandomArbitrary(1, 255);
+
+  while (reserveIPs.find((i) => i.ip == targetIP)) {
+    targetIP = getRandomArbitrary(1, 255);
+  }
+
+  reserveIPs.push({
+    targetIP,
+    targetPass
+  });
+
+  res.send({
+    success: true,
+    pass: targetPass
+  });
+});
 
 const wss = new WebSocketServer({
   server: server,
