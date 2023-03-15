b3af8f7dbcf7f725cb0962a7530947027be4cd25
greysoh <dev@hofers.cloud>
2023-03-15 15:24:51 -0400

    chore(ws-server): Formats code.

--- ws-server/index.mjs
+++ ws-server/index.mjs
@@ -4,6 +4,8 @@ import { WebSocketServer } from "ws";
 import { createServer } from "http";
 import { readFile } from "fs/promises";
 
+import { handleMessaage } from "./MessageServer/index.mjs";
+
 const config = JSON.parse(await readFile("./config.json"));
 
 const clients = [];
@@ -23,21 +25,21 @@ function canBeParsedAsJSON(...data) {
     try {
       JSON.parse(data);
     } catch (e) {
-      return false; 
+      return false;
     }
   }
 
   return true;
 }
 
-app.get("/api/v1/getClientList", function(req, res) {
+app.get("/api/v1/getClientList", function (req, res) {
   res.send({
     success: true,
-    data: clients // TODO: sanitize(?)
-  })
+    data: clients, // TODO: sanitize(?)
+  });
 });
 
-app.post("/api/v1/reserveClientIP", function(req, res) {
+app.post("/api/v1/reserveClientIP", function (req, res) {
   const targetPass = getRandomArbitrary(10000000, 99999999);
   let targetIP = Math.floor(getRandomArbitrary(1, 255));
 
@@ -47,128 +49,166 @@ app.post("/api/v1/reserveClientIP", function(req, res) {
 
   reserveIPs.push({
     targetIP,
-    targetPass
+    targetPass,
   });
 
   res.send({
     success: true,
     pass: targetPass,
-    ip: targetIP
+    ip: targetIP,
   });
 });
 
 const wss = new WebSocketServer({
   server: server,
-  path: "/ws"
+  path: "/ws",
 });
 
-wss.on("connection", function(ws) {
+wss.on("connection", function (ws) {
   ws.clientConfig = {}; // Client configuration data (ex. IP data)
 
-  ws.on("message", function(msg) {
-    if (!ws.clientConfig.allDataPassed) {
-      if (!canBeParsedAsJSON(msg)) {
-        return ws.send(JSON.stringify({
+  ws.on("message", function (msg) {
+    if (ws.clientConfig.allDataPassed) {
+      // Begin message handle
+      handleMessaage(msg, ws.clientConfig, clients);
+    }
+
+    if (!canBeParsedAsJSON(msg)) {
+      return ws.send(
+        JSON.stringify({
           success: false,
-          error: "Data unable to be parsed as JSON."
-        }));
+          error: "Data unable to be parsed as JSON.",
+        })
+      );
+    }
+
+    const data = JSON.parse(msg);
+
+    if (!ws.clientConfig.targetPass) {
+      // Check if we have the password for the account
+      if (data.mode != "authenticate") {
+        return ws.send(
+          JSON.stringify({
+            success: false,
+            error: "Not authenticated.",
+          })
+        );
       }
 
-      const data = JSON.parse(msg);
+      const item = reserveIPs.find((i) => i.targetPass == data.pass);
 
-      if (!ws.clientConfig.targetPass) { // Check if we have the password for the account
-        if (data.mode != "authenticate") {
-          return ws.send(JSON.stringify({
+      if (!item) {
+        return ws.send(
+          JSON.stringify({
             success: false,
-            error: "Not authenticated."
-          }));
-        }
-
-        const item = reserveIPs.find((i) => i.targetPass == data.pass);
-
-        if (!item) {
-          return ws.send(JSON.stringify({
-            success: false,
-            error: "Invalid password."
-          }));
-        }
-
-        ws.clientConfig = item;
-        
-        return ws.send(JSON.stringify({
-          success: true
-        }))
+            error: "Invalid password.",
+          })
+        );
       }
 
-      switch (data.mode) {
-        default: {
-          return ws.send(JSON.stringify({
-            success: false,
-            error: "Invalid mode specified"
-          }))
-        }
+      ws.clientConfig = item;
 
-        case "getSysConfigInfo": {
-          return ws.send(JSON.stringify({
+      return ws.send(
+        JSON.stringify({
+          success: true,
+        })
+      );
+    }
+
+    switch (data.mode) {
+      default: {
+        return ws.send(
+          JSON.stringify({
+            success: false,
+            error: "Invalid mode specified",
+          })
+        );
+      }
+
+      case "getSysConfigInfo": {
+        return ws.send(
+          JSON.stringify({
             success: true,
-            data: ws.clientConfig
-          }))
-        }
+            data: ws.clientConfig,
+          })
+        );
+      }
 
-        case "listen": {
-          if (!data.port) return ws.send(JSON.stringify({
-            success: false,
-            error: "Missing port"
-          }))
+      case "listen": {
+        if (!data.port)
+          return ws.send(
+            JSON.stringify({
+              success: false,
+              error: "Missing port",
+            })
+          );
 
-          if (data.port <= 0 || data.port > 65535) return ws.send(JSON.stringify({
-            success: false,
-            error: "Port out of range"
-          }));
+        if (data.port <= 0 || data.port > 65535)
+          return ws.send(
+            JSON.stringify({
+              success: false,
+              error: "Port out of range",
+            })
+          );
 
-          ws.clientConfig.mode = "listen";
-          ws.clientConfig.port = data.port;
+        ws.clientConfig.mode = "listen";
+        ws.clientConfig.port = data.port;
 
-          ws.clientConfig.allDataPassed = true;
+        ws.clientConfig.allDataPassed = true;
 
-          return ws.send(JSON.stringify({
+        return ws.send(
+          JSON.stringify({
             success: true,
-            message: "Switched modes from Console to Listen"
-          }));
-        }
+            message: "Switched modes from Console to Listen",
+          })
+        );
+      }
 
-        case "connect": {
-          if (!data.port) return ws.send(JSON.stringify({
-            success: false,
-            error: "Missing port"
-          }))
+      case "connect": {
+        if (!data.port)
+          return ws.send(
+            JSON.stringify({
+              success: false,
+              error: "Missing port",
+            })
+          );
 
-          if (!data.hostID) return ws.send(JSON.stringify({
-            success: false,
-            error: "Missing host ID"
-          }))
+        if (!data.hostID)
+          return ws.send(
+            JSON.stringify({
+              success: false,
+              error: "Missing host ID",
+            })
+          );
 
-          if (data.port <= 0 || data.port > 65535) return ws.send(JSON.stringify({
-            success: false,
-            error: "Port out of range"
-          }));
+        if (data.port <= 0 || data.port > 65535)
+          return ws.send(
+            JSON.stringify({
+              success: false,
+              error: "Port out of range",
+            })
+          );
 
-          if (data.port <= 0 || data.port > 255) return ws.send(JSON.stringify({
-            success: false,
-            error: "Host ID out of range"
-          }));
+        if (data.port <= 0 || data.port > 255)
+          return ws.send(
+            JSON.stringify({
+              success: false,
+              error: "Host ID out of range",
+            })
+          );
 
-          ws.clientConfig.allDataPassed = true;
+        ws.clientConfig.allDataPassed = true;
 
-          return ws.send(JSON.stringify({
+        return ws.send(
+          JSON.stringify({
             success: true,
-            message: "Switched modes from Console to Connect"
-          }));
-        }
+            message: "Switched modes from Console to Connect",
+          })
+        );
       }
     }
   });
-}); 
+});
 
 server.listen(config.port ? config.port : 1280);
 console.log("Listening on %s", config.port ? config.port : 1280);
